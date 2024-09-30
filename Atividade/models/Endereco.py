43ee036482eb85c9ced1ec8dc2a0453725dbf0ef
Annfranco <160090372+Annfranco@users.noreply.github.com>
2024-09-30 12:56:51 +0000

    Update

---
+++ Atividade/models/Endereco.py
@@ -0,0 +1,11 @@
+class Endereco:
+    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+
+    def __str__(self) -> str:
+        return (f"\nLogradouro: {self.logradouro}"
+                f"\nNumero: {}")
