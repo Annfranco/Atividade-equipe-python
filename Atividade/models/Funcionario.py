80ef8567ce3059aca29590d228d5b9789fee5130
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-09-30 13:02:11 +0000

    update

--- Atividade/models/Funcionario.py
+++ Atividade/models/Funcionario.py
@@ -1,10 +1,16 @@
-from atividade.models.endereco import Endereco
+from abc import abstractmethod
+from Atividade.models.Endereco import Endereco
 
-class Funcionario():
+class Funcionario(abstractmethod):
     def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco) -> None:
         self.nome = nome
         self.telefone = telefone
         self.email = email
         self.endereco = endereco
 
-        def str
+    def __str__(self) -> str:
+        return(f"== Funcionário =="
+               f"\nNome - {self.nome}"
+               f"\nTelefone - {self.telefone}"
+               f"\nE-mail - {self.email}"
+               f"\n== Endereço ==")
