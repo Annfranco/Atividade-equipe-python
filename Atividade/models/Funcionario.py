8b5868eaf3f8b2498d72643a06a37fe933cc566d
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-09-30 12:53:46 +0000

    incompleto

---
+++ Atividade/models/Funcionario.py
@@ -0,0 +1,10 @@
+from atividade.models.endereco import Endereco
+
+class Funcionario():
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco) -> None:
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.endereco = endereco
+
+        def str
