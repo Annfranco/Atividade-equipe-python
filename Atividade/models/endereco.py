0079689dee2354e1641d03b37792eb052bf15a4b
Annfranco <160090372+Annfranco@users.noreply.github.com>
2024-10-07 12:38:09 +0000

    update

--- Atividade/models/endereco.py
+++ Atividade/models/endereco.py
@@ -1,46 +1,102 @@
 class Endereco:
     def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str) -> None:
-        self.logradouro = self.__verificar_logradouro_endereco(logradouro)
-        self.numero = self.__verificar_numero_endereco(numero)
-        self.complemento = self.__verificar_complemento_endereco(complemento)
-        self.cep = self.__verificar_cep_endereco(cep)
-        self.cidade = self.__verificar_cidade_endereco(cidade)
+        
+        self.logradouro = self._verificar_logradouro_endereco(logradouro)
+        self.numero = self._verificar_numero_endereco(numero)
+        self.complemento = self._verificar_complemento_endereco(complemento)
+        self.cep = self._verificar_cep_endereco(cep)
+        self.cidade = self._verificar_cidade_endereco(cidade)
 
-    def __verificar_logradouro_endereco(self, logradouro):
+    def _verificar_logradouro_endereco(self,logradouro):
+
+        self._verificar_logradouro_invalido(logradouro)
+        self._verificar_logradouro_vazio(logradouro)
+
+        self.logradouro = logradouro
+        return self.logradouro
+    
+    def _verificar_numero_endereco(self, numero):
+
+        self._verificar_numero_invalido(numero)
+        self._verificar_numero_vazio(numero)
+
+        self.numero = numero
+        return self.numero
+    
+    def _verificar_complemento_endereco(self, complemento):
+
+        self._verificar_complemento_invalido(complemento)
+        self._verificar_complemento_vazio(complemento)
+
+        self.complemento = complemento
+        return self.complemento
+    
+    def _verificar_cep_endereco(self, cep):
+
+        self._verificar_cep_invalido(cep)
+        self._verificar_cep_vazio(cep)
+
+        self.cep = cep
+        return self.cep
+    
+    def _verificar_cidade_endereco(self, cidade):
+
+        self._verificar_cidade_invalido(cidade)
+        self._verificar_cidade_vazio(cidade)
+
+        self.cidade = cidade
+        return self.cidade
+    
+    def _verificar_logradouro_vazio(self, logradouro):
         if not logradouro.strip():
             raise ValueError("O que está sendo solicitado está vazio.")
-        if not isinstance(logradouro, str):
-            raise TypeError("valor inválido.")
         return logradouro
-    
-    def __verificar_numero_endereco(self, numero):
+        
+    def _verificar_logradouro_invalido(self,logradouro):
+        if not isinstance(logradouro, str):
+            raise TypeError("O que está sendo solicitado está invalido.")
+        return logradouro
+
+    def _verificar_numero_vazio(self, numero):
         if not numero.strip():
             raise ValueError("O que está sendo solicitado está vazio.")
-        if not isinstance(numero, str):
-            raise TypeError("valor inválido.")
         return numero
     
-    def __verificar_complemento_endereco(self, complemento):
+    def _verificar_numero_invalido(self, numero):
+        if not isinstance(numero, str):
+            raise TypeError("O que está sendo solicitado está invalido.")
+        return numero
+    
+    def _verificar_complemento_vazio(self, complemento):
         if not complemento.strip():
             raise ValueError("O que está sendo solicitado está vazio.")
-        if not isinstance(complemento, str):
-            raise TypeError("valor inválido.")
         return complemento
     
-    def __verificar_cep_endereco(self, cep):
+    def _verificar_complemento_invalido(self, complemento):
+        if not isinstance(complemento, str):
+            raise TypeError("O que está sendo solicitado está invalido.")
+        return complemento
+    
+    def _verificar_cep_vazio(self, cep):
         if not cep.strip():
             raise ValueError("O que está sendo solicitado está vazio.")
+        return cep 
+    
+    def _verificar_cep_invalido(self, cep):
         if not isinstance(cep, str):
-            raise TypeError("valor inválido.")
+            raise TypeError("O que está sendo solicitado está invalido.")
         return cep
     
-    def __verificar_cidade_endereco(self, cidade):
+    def _verificar_cidade_vazio(self, cidade):
         if not cidade.strip():
             raise ValueError("O que está sendo solicitado está vazio.")
-        if not isinstance(cidade, str):
-            raise TypeError("valor inválido.")
         return cidade
-
+    
+    def _verificar_cidade_invalido(self, cidade):
+        if not isinstance(cidade, str):
+            raise TypeError("O que está sendo solicitado está invalido.")
+        return cidade
+    
     def __str__(self) -> str:
         return (f"\nLogradouro: {self.logradouro}"
                 f"\nNumero: {self.numero}"
--- Atividade/models/tests/test_endereco.py
+++ Atividade/models/tests/test_endereco.py
@@ -26,18 +26,38 @@ def test_endereco_logradouro_vazio_retorna_mensagem_erro():
     with pytest.raises(ValueError, match="O que está sendo solicitado está vazio."):
         Endereco("", "210", "Terreo", "4100-210", "Salvador")
 
+def test_endereco_logradouro_invalido_erro():
+    with pytest.raises(TypeError, match="O que está sendo solicitado está invalido."):
+        Endereco(1165, "210", "Terreo", "4100-210", "Salvador")
+
 def test_endereco_numero_vazio_retorna_mensagem_erro():
     with pytest.raises(ValueError, match="O que está sendo solicitado está vazio."):
         Endereco("Rua A", "", "Terreo", "4100-210", "Salvador")
+
+def test_endereco_numero_invalido_erro():
+    with pytest.raises(TypeError, match="O que está sendo solicitado está invalido."):
+        Endereco("Rua A", 111, "Terreo", "4100-210", "Salvador")
 
 def test_endereco_complemento_vazio_retorna_mensagem_erro():
     with pytest.raises(ValueError, match="O que está sendo solicitado está vazio."):
         Endereco("Rua A", "210", "", "4100-210", "Salvador")
 
+def test_endereco_complemento_invalido_erro():
+    with pytest.raises(TypeError, match="O que está sendo solicitado está invalido."):
+        Endereco("Rua A", "210", 111, "4100-210", "Salvador")
+
 def test_endereco_cep_vazio_retorna_mensagem_erro():
     with pytest.raises(ValueError, match="O que está sendo solicitado está vazio."):
         Endereco("Rua A", "210", "Terreo", "", "Salvador")
 
+def test_endereco_cep_invalido_erro():
+    with pytest.raises(TypeError, match="O que está sendo solicitado está invalido."):
+        Endereco("Rua A", "210", "Terreo", 111, "Salvador")
+
 def test_endereco_cidade_vazio_retorna_mensagem_erro():
     with pytest.raises(ValueError, match="O que está sendo solicitado está vazio."):
         Endereco("Rua A", "210", "Terreo", "4100-210", "")
+
+def test_endereco_cidade_invalido_erro():
+    with pytest.raises(TypeError, match="O que está sendo solicitado está invalido."):
+        Endereco("Rua A", "210", "Terreo", "4100-210", 111)
